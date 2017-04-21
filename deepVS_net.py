9ad3e51c81ba5400c6bdc16c30640d1c12e7074d
Brian Xie <brianxie@mit.edu>
2017-04-21 15:53:51 -0400

    changed python preprocessing to tensorflow

--- deepVS_net.py
+++ deepVS_net.py
@@ -38,22 +38,33 @@ def bias_variable(shape):
     initial = tf.constant(0.01, shape=shape)
     return tf.Variable(initial)
 
-def variable_summaries(var, name):
-    """attaches a lot of summaries to a tensor."""
-    with tf.name_scope('summaries'):
-        mean = tf.reduce_mean(var)
-        tf.summary.scalar('mean/' + name, mean)
-    with tf.name_scope('stddev'):
-        stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-        tf.summary.scalar('stddev/' + name, stddev)
-        tf.summary.scalar('max/' + name, tf.reduce_max(var))
-        tf.summary.scalar('min/' + name, tf.reduce_min(var))
-        tf.summary.histogram(name, var)
+def preprocess_layer(layer_name, atoms, coords):
+	#get a matrix of distances between atoms (tensor with shape [m, m])
+	coords_copy = tf.transpose(tf.expand_dims(coords, 0), perm=[1,0,2])
+	distances = tf.reduce_sum(tf.square(coords - coords_copy), reduction_indices=[2]) ** 0.5
+	
+	#get the closest neighbors using tf.nn.top_k, and the corresponding atoms
+	neg_distances = tf.multiply(distances, tf.constant(-1.0))
+	neighbor_neg_distances, neighbor_atom_indices = tf.nn.top_k(neg_distances, k_c)
+	#binning the distances and associating atom indices with actual atom types
+	neighbor_distances = tf.multiply(neighbor_neg_distances, tf.constant(-1.0/DIST_INTERVAL))
+	neighbor_distances = tf.add(neighbor_distances, tf.constant(1.0))
+	neighbor_atoms = tf.gather(atoms, neighbor_atom_indices)
+	#convert to int, since we'll need that for tf.nn.embedding_lookup
+	neighbor_distances = tf.to_int32(neighbor_distances)
+
+	#combine the neighbor atoms and neighbor distances
+	z = tf.stack([neighbor_atoms, neighbor_distances], axis=1)
+	#reshape our tensor to get shape [k_c, 2, m]
+	z_transposed = tf.transpose(z, perm=[2, 1, 0])
+
+	print layer_name, "output dimensions:", z_transposed.get_shape()
+	return z_transposed
 
 def embed_layer(layer_name, input_tensor):
 	"""performs feature embedding on the input tensor, 
 	transforming numbers to 200D vectors.
-	Input: tensor of shape k_c * 2 * m
+	Input: tensor of shape [k_c, 2, m]
 	Output: tensor of shape [k_c, d_dist + d_atm, m, 1]
 	"""
 	with tf.name_scope(layer_name):
@@ -75,12 +86,9 @@ def conv_layer(layer_name, input_tensor, filter_size, strides=[1,1,1,1,1], paddi
 	with tf.name_scope(layer_name):
 		with tf.name_scope('weights'):
 			W_conv = weight_variable(filter_size)
-			variable_summaries(W_conv, layer_name + '/weights')
 		with tf.name_scope('biases'):
 			b_conv = bias_variable([filter_size[3]])
-			variable_summaries(b_conv, layer_name + '/biases')
 		h_conv = tf.nn.conv3d(input_tensor, W_conv, strides=strides, padding=padding) + b_conv
-		tf.summary.histogram(layer_name + '/pooling_output', h_conv)
 	print layer_name,"output dimensions:", h_conv.get_shape()
 	return h_conv
 	
@@ -91,81 +99,21 @@ def fc_layer(layer_name,input_tensor,output_dim):
 
 	with tf.name_scope(layer_name):
 		weights = weight_variable([input_dim, output_dim])
-		variable_summaries(weights, layer_name + '/weights')
 	with tf.name_scope('biases'):
 		biases = bias_variable([output_dim])
-		variable_summaries(biases, layer_name + '/biases')
 	with tf.name_scope('Wx_plus_b'):
 		h_fc = tf.matmul(input_tensor, weights) + biases
-		tf.summary.histogram(layer_name + '/fc_output', h_fc)
 	print layer_name, "output dimensions:", h_fc.get_shape()
 	return h_fc
 
 #-----------------------------------NETWORK----------------------------------------#
 
-class Z(object):
-
-	def __init__(self, ligand_atoms, ligand_coords):
-		""" Takes input ligand_atoms, ligand_coords in the form of lists. Transforms ligand_coords into 
-		a dictionary and constructs z, a [m * kc * 2] matrix. """
-		self.ligand_atoms = ligand_atoms
-		ligand_coords_dict = {}
-		for i in range(len(ligand_atoms)):
-			ligand_coords_dict[i] = ligand_coords[i]
-		self.ligand_coords = ligand_coords_dict
-		self.z = self.build_z()
-
-	def build_z(self):
-		#returns matrix of dimensions [m * kc * 2]
-		raw_z = self.get_raw_z()
-		for atom_index in range(len(raw_z)): #iterate over atoms dimension (m)
-			for neighbor_index in range(len(raw_z[0])): #iterate over neighbors (kc)
-				atom, distance = raw_z[atom_index][neighbor_index]
-				raw_z[atom_index][neighbor_index][0] = self.ligand_atoms[raw_z[atom_index][neighbor_index][0]]
-				raw_z[atom_index][neighbor_index][1] = int(distance//DIST_INTERVAL + 1)
-		return raw_z
-
-	def get_raw_z(self):
-		#returns matrix of dimensions [m * kc * 2]
-		raw_z = []
-		for atom_index in range(len(self.ligand_atoms)):
-			kc_neighbors_dict = self.get_closest_atoms_and_distances(atom_index)
-			kc_neighbors_list = [[neighbor, distance] for neighbor, distance in kc_neighbors_dict.items()]
-			kc_neighbors_list.sort(key=lambda x: x[1]) #sort by distance
-			raw_z.append(kc_neighbors_list) 
-		return raw_z
-
-	def get_closest_atoms_and_distances(self, atom_index):
-		distances = self.convert_coords_to_distances(atom_index)
-		closest = {}
-		for _ in range(k_c):
-			closest_atom = min(distances, key=distances.get)
-			closest[closest_atom] = distances[closest_atom]
-			del distances[closest_atom]
-		return closest
-
-	def convert_coords_to_distances(self, atom_index):
-		ligand_distances = {}
-		atom_coord = self.ligand_coords[atom_index]
-		for neighbor in self.ligand_coords:
-			ligand_distances[neighbor] = self.distance(self.ligand_coords[neighbor], atom_coord)
-		return ligand_distances
-
-	def distance(self, coord1, coord2):
-		x1, y1, z1 = coord1
-		x2, y2, z2 = coord2
-		return ((x1-x2) ** 2 + (y1-y2) ** 2 + (z1-z2) ** 2) ** 0.5
-
-
-def construct_z(ligand_atoms, ligand_coords):
-	""" Returns the z, that has not done feature embedding. Returns array. """
-	return Z(ligand_atoms, ligand_coords).z
-
-
-def deepVS_net(input_tensor, keep_prob):
-	"""input_tensor is a tensor of shape [k_c * 2 * m]"""
+def deepVS_net(ligand_atoms, ligand_coords, keep_prob):
+	"""ligand_atoms has dimensions [m], ligand_coords has dimensions [m, 3]"""
+	#take ligand_atoms, ligand_coords, preprocess it into our z vector
+	z_processed = preprocess_layer('preprocess', ligand_atoms, ligand_coords)
 	#do the feature embedding to get a tensor with shape [k_c, d_dist+d_atm, m, 1]
-	z_embed = embed_layer('embedding', input_tensor)
+	z_embed = embed_layer('embedding', z_processed)
 	#convolutional layer - padding = 'VALID' prevents 0 padding
 	z_conv = conv_layer('face_conv', input_tensor=z_embed, filter_size=[k_c, d_atm+d_dist, 1, 1, cf], padding='VALID')
 	#max pool along the columns (corresponding to each convolutional filter)
